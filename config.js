634ec0310c4d2070f97cc75d0d164a6485e69175
bbsabaliya-creator <bbsabaliya@gmail.com>
2026-02-04 14:45:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,69 @@ const CONFIG = {
 
   week: {
 
-    7: { name: "Rose Day 🌹", float: "🌹🌹🌹" },
-    8: { name: "Propose Day 💍", float: "💍💍💍" },
-    9: { name: "Chocolate Day 🍫", float: "🍫🍫🍫" },
-    10:{ name: "Teddy Day 🧸", float: "🧸🧸🧸" },
-    11:{ name: "Promise Day 🤝", float: "🤝🤝🤝" },
-    12:{ name: "Hug Day 🤗", float: "🤗🤗🤗" },
-    13:{ name: "Kiss Day 💋", float: "💋💋💋" },
-    14:{ name: "Valentine Day ❤️", float: "❤️❤️❤️" }
+"02-07": {
+      name: "Rose Day 🌹",
+      emoji: "🌹",
+      symbol: "🌹",
+      title: "Happy Rose Day My Love",
+      final: "My love blooms for you 🌹💖"
+    },
+
+    "02-08": {
+      name: "Propose Day 💍",
+      emoji: "💍",
+      symbol: "💍",
+      title: "Happy Propose Day My Love",
+      final: "Will you be mine forever? 💍💘"
+    },
+
+    "02-09": {
+      name: "Chocolate Day 🍫",
+      emoji: "🍫",
+      symbol: "🍫",
+      title: "Happy Chocolate Day My Sweetheart",
+      final: "You are sweeter than chocolate 🍫💖"
+    },
+
+    "02-10": {
+      name: "Teddy Day 🧸",
+      emoji: "🧸",
+      symbol: "🧸",
+      title: "Happy Teddy Day My Cutie",
+      final: "You are my favorite teddy 🧸💘"
+    },
+
+    "02-11": {
+      name: "Promise Day 🤞",
+      emoji: "⭐",
+      symbol: "⭐",
+      title: "Happy Promise Day My Love",
+      final: "I promise you my forever 🤞💖"
+    },
+
+    "02-12": {
+      name: "Hug Day 🤗",
+      emoji: "🤗",
+      symbol: "💞",
+      title: "Happy Hug Day My Life",
+      final: "My arms are your home 🤗❤️"
+    },
+
+    "02-13": {
+      name: "Kiss Day 😘",
+      emoji: "💋",
+      symbol: "💋",
+      title: "Happy Kiss Day My Heart",
+      final: "Your kisses are my addiction 😘💖"
+    },
+
+    "02-14": {
+      name: "Valentine Day ❤️",
+      emoji: "❤️",
+      symbol: "❤️",
+      title: "Happy Valentine’s Day My Forever",
+      final: "You are my today, tomorrow, always ❤️💍"
+    }
 
   }
 
