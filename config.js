de4882e2c4259c139612b6b3dcedbc81f1ea8099
bbsabaliya-creator <bbsabaliya@gmail.com>
2026-02-05 18:29:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ Bhargav 💍💘
 
   week: {
 
-    "02-07": {
+    "02-06": {
         name: "Rose Day 🌹",
         emoji: "🌹",
         symbol: "🌹",
@@ -27,7 +27,7 @@ Bhargav 💍💘
         final: "My love blooms for you 🌹💖"
     },
 
-    "02-08": {
+    "02-07": {
         name: "Propose Day 💍",
         emoji: "💍",
         symbol: "💍",
